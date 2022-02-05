ade0cdc2eae595033be31ab7a6594cc7fcb907b0
EnricoWSB <99039489+EnricoWSB@users.noreply.github.com>
2022-02-05 13:25:03 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -8,7 +8,7 @@ output "database_name" {
 
 output "connection_string" {
   description = "Connection string for the Azure SQL Database created."
-  value = "Server=tcp:${azurerm_sql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.db.name};Persist Security Info=False;User ID=${var.sql_admin_username};Password=${var.sql_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  value = "Server=tcp:${azurerm_sql_server.appdb.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.db.name};Persist Security Info=False;User ID=${var.sql_admin_username};Password=${var.sql_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
   sensitive   = true
 }
 
