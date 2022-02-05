6a96bf038115d23ea03a3598cfa449ac27fc0222
EnricoWSB <99039489+EnricoWSB@users.noreply.github.com>
2022-02-05 13:35:17 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,36 +9,36 @@ variable "resource_group_location" {
 }
 
 variable "sql_admin_username" {
-   description = "The administrator username of the SQL Server."
-   default = "enrico"
+  description = "The administrator username of the SQL Server."
+  default     = "enrico"
 }
 
 variable "sql_password" {
-   description = "The administrator password of the SQL Server."
-   default = "Hta5SFVEZ75r#NpX"
+  description = "The administrator password of the SQL Server."
+  default     = "Hta5SFVEZ75r#NpX"
 }
 
 variable "vm_password" {
-   description = "The administrator password of the VM."
-   default = "Hta5SFVEZ75r#NpX"
+  description = "The administrator password of the VM."
+  default     = "Hta5SFVEZ75r#NpX"
 }
 
 variable "sql_server_name" {
-   description = "Name of the SQL server."
-   default = "sqlservershoplistwebapp"
+  description = "Name of the SQL server."
+  default     = "sqlservershoplistwebapp"
 }
 
 variable "sql_database_name" {
-   description = "Name of the SQL Database."
-   default = "sqldatabaseshoplistwebapp"
+  description = "Name of the SQL Database."
+  default     = "sqldatabaseshoplistwebapp"
 }
 
 variable "storage_account_name" {
-   description = "Name of the storage account name."
-   default = "storageaccshoplist"
+  description = "Name of the storage account name."
+  default     = "storageaccshoplist"
 }
 
 variable "app_service_name" {
-   description = "Name of the app server."
-   default = "appserviceshoplistwebappenrico"
+  description = "Name of the app server."
+  default     = "appserviceshoplistwebappenrico"
 }
